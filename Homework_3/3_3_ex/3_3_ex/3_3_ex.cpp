05206f2a66e4c109605014ec3920c2ff324f3dd8
Maria-Miroshnikova <mari.mirosh@bk.ru>
2018-10-11 21:02:25 +0300

    add output and input

--- Homework_3/3_3_ex/3_3_ex/3_3_ex.cpp
+++ Homework_3/3_3_ex/3_3_ex/3_3_ex.cpp
@@ -3,8 +3,40 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+
+void pyramidSort(int length, int sortArray[])
+{
+
+}
+
+void inputArray(int length, int block[])
+{
+	for (int i = 0; i < length; ++i)
+	{
+		scanf("%d", &block[i]);
+	}
+}
+
+void outputArray(int length, int block[])
+{
+	for (int i = 0; i < length; ++i)
+	{
+		printf("%d ", block[i]);
+	}
+}
 
 int main()
 {
+	int length = 0;
+	scanf("%d", &length);
+	int *sortArray = new int[length] {};
+	inputArray(length, sortArray);
+	printf("Array:\n");
+	outputArray(length, sortArray);
+	printf("\n");
+	pyramidSort(length, sortArray);
+	delete[] sortArray;
+	system("pause");
 	return 0;
 }
