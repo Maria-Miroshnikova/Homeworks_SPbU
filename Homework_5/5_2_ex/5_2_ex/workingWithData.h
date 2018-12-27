8e0d6d89666fbbb844ec000dcccd46dc2b8a1571
Maria Miroshnikova <mari.mirosh@bk.ru>
2018-12-27 07:06:26 +0300

    Delete workingWithData.h

--- Homework_5/5_2_ex/5_2_ex/workingWithData.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/// рассчитывает позицию выжившего.
-int findTheSurvivor(int countMen, int numberToKill);
-
-/// выводит приветствие, принимает данные, посылает запрос на рассчет
-/// позиции выжевшего и выводит результат на экран.
-void workingWithData();
